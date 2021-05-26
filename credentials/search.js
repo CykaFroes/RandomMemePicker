7dd5fbc30b9c9e93f2b367e6f800df2e52c6c854
CykaFroes <nikowluiz@gmail.com>
2021-05-26 18:15:23 -0300

    version 0.0.2

---
+++ credentials/search.js
@@ -0,0 +1,6 @@
+var data = {
+    "apiKey": "AIzaSyCvPDnXxklISSh0TSauwSEm8kLiytHB93Q",
+    "searchEngineIdEn": "70262f8184eb50c44",
+    "searchEngineIdPt": "1a74397a6b8cf4aa9",
+    "searchEngineIdDevEn": "98f000099e5239fad"
+};
